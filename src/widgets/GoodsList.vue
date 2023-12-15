8537f0fda947ca312ba364a75a8a114600947696
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-15 19:11:41 +0300

    add GoodsList component. The component allows you to display a list of goods cards in a specified number of columns

---
+++ src/widgets/GoodsList.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul
+    :style="listDynamicStyles"
+    class="goods-list"
+  >
+    <li
+      v-for="goods in goodsList"
+      :key="goods.id"
+      class="goods-list__item"
+    >
+      <RouterLink to="/">
+        <GoodsCard
+          :img="goods.img"
+          :alt="goods.alt"
+          :description="goods.description"
+          :price="goods.price"
+          :title="goods.title"
+          class="goods-list__card"
+        />
+      </RouterLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import GoodsCard from '@/blocks/GoodsCard.vue';
+import type { GoodsCardDto } from '@/types/goodsCard';
+import { computed } from 'vue';
+
+const props = defineProps<{
+  goodsList: GoodsCardDto[];
+  cols: number;
+}>();
+
+const listDynamicStyles = computed(() => ({
+  'grid-template-columns': `repeat(${props.cols}, 1fr)`
+}));
+</script>
+
+<style scoped lang="scss">
+.goods-list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 15px;
+
+  &__card {
+    height: 100%;
+  }
+}
+</style>

bd4c68300fd101c42292e94db44bdaa0bb7fe783
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-15 21:24:49 +0300

    add JmHeader component

---
+++ src/widgets/JmHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header"><slot /></header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: teal;
+  padding: $padding-v $padding-h;
+  background-color: var(--cl-block);
+  border-bottom: 1px solid var(--cl-border);
+}
+</style>

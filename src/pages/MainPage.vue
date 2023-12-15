411ba20407ba7703f6f7b8bfc3c9420f3aa907f2
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-15 20:39:43 +0300

    add the GoodsList to the main page with mock data

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>Main Page</div>
+  <div>
+    <GoodsList
+      :goods-list="data"
+      :cols="5"
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GoodsList from '@/widgets/GoodsList.vue';
+import goodsData from '@/mocks/goods';
 
-<style lang="scss" scoped></style>
+const data = goodsData;
+</script>

7a06ef16a86e9b688ba3f886ffb4d860f3921e26
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-15 18:56:36 +0300

    add base wrapper for a Card component

---
+++ src/components/JmCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="jm-card">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.jm-card {
+  background-color: var(--cl-block);
+  padding: $padding-v $padding-h;
+  border-radius: $border-radius;
+}
+</style>

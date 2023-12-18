57586725e9f9dac15a7d1d0b8a3a9e17d8db5319
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-18 17:01:34 +0300

    style: add semicolons in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 import { setScrollBarWidth } from './utils';
 
-const app = createApp(App)
 setScrollBarWidth();
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');

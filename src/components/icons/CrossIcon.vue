50183c3d559fb1caaa18b9124eab968a1bb4d7c2
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-18 17:45:16 +0300

    add catalog and cross svg icons

---
+++ src/components/icons/CrossIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-width="2"
+      vector-effect="non-scaling-stroke"
+      d="M20 20L4 4m16 0L4 20"
+    />
+  </svg>
+</template>

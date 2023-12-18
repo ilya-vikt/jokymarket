d1c9e7b57b2e3e13b4c06dee55a12ce95e111599
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-18 17:14:32 +0300

    add Sceleton Loader block for use in the lists

---
+++ src/blocks/SceletonLoader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="sceleton-loader"
+    :class="{
+      'sceleton-loader--grid': grid
+    }"
+  >
+    <JmLoader
+      v-for="idx in blockCount"
+      :key="idx"
+      class="sceleton-loader__block"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import JmLoader from '@/components/JmLoader.vue';
+withDefaults(
+  defineProps<{
+    grid?: boolean;
+    blockHeight?: string;
+    cols?: number;
+    blockCount: number;
+  }>(),
+  {
+    cols: 1,
+    blockHeight: 'calc(1.2rem + 20px)'
+  }
+);
+</script>
+
+<style scoped lang="scss">
+.sceleton-loader {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  &__block {
+    height: v-bind(blockHeight);
+  }
+
+  &--grid {
+    display: grid;
+    grid-template-columns: repeat(v-bind(cols), 1fr);
+  }
+}
+</style>

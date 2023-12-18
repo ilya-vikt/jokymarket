1de796b266c00faf7d60419226d4785e1de913ba
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-18 16:14:38 +0300

    refactor: rename file vars.scss to share.scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,9 +15,9 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-                @import "@/assets/styles/vars.scss"; 
+                @import "@/assets/styles/share.scss"; 
             `
       }
     }
   }
-})
+});

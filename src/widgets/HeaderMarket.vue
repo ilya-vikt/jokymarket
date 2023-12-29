cb083af35fab78be3dfc3dd1f4b810f84a7ec4fc
Ilya Viktorov <ilya-vikt@yandex.ru>
2023-12-29 16:56:34 +0300

    add adaptive to the HeaderMarket

--- src/widgets/HeaderMarket.vue
+++ src/widgets/HeaderMarket.vue
@@ -6,16 +6,32 @@
           :icon="CatalogIcon"
           has-border
           has-hover
-          :pushed="catalogOpened"
-          @click="catalogOpened = !catalogOpened"
+          :pushed="catalogStore.opened"
+          @click="catalogToggle"
         >
           Каталог
         </JmButton>
       </div>
-
-      <NavbarHeader class="market-header__navbar" />
+      <JmButton
+        v-if="globalStore.isCompact"
+        :icon="globalStore.droverOpened ? CrossIcon : BurgerIcon"
+        has-hover
+        class="market-header__burger"
+        @click="menuToggle"
+      ></JmButton>
+      <NavbarHeader
+        v-if="!globalStore.isCompact"
+        class="market-header__navbar"
+      />
     </div>
     <GoodsCatalog />
+    <DroverBase
+      v-if="globalStore.droverOpened && globalStore.isCompact"
+      :panel-width="droverWidth"
+      @closeDrover="globalStore.droverOpened = false"
+    >
+      <BurgerMenu />
+    </DroverBase>
   </HeaderBase>
 </template>
 
@@ -26,9 +42,41 @@ import GoodsCatalog from '@/blocks/GoodsCatalog/GoodsCatalog.vue';
 import JmButton from '@/components/JmButton.vue';
 import CatalogIcon from '@/components/icons/CatalogIcon.vue';
 import { useCatalogStore } from '@/stores/catalogStore';
-import { storeToRefs } from 'pinia';
+import BurgerIcon from '@/components/icons/BurgerIcon.vue';
+import CrossIcon from '@/components/icons/CrossIcon.vue';
+import { useGlobalStore } from '@/stores/globalStore';
+import { computed, watch } from 'vue';
+import DroverBase from '@/blocks/DroverBase.vue';
+import BurgerMenu from '@/blocks/BurgerMenu.vue';
 
-const { opened: catalogOpened } = storeToRefs(useCatalogStore());
+const catalogStore = useCatalogStore();
+const globalStore = useGlobalStore();
+const droverWidth = computed(() => (globalStore.isMobile ? '100%' : '50%'));
+
+const catalogToggle = () => {
+  if (globalStore.droverOpened && !catalogStore.opened) {
+    globalStore.droverOpened = false;
+  }
+
+  catalogStore.opened = !catalogStore.opened;
+};
+
+const menuToggle = () => {
+  if (catalogStore.opened && !globalStore.droverOpened) {
+    catalogStore.opened = false;
+  }
+
+  globalStore.droverOpened = !globalStore.droverOpened;
+};
+
+watch(
+  () => globalStore.isCompact,
+  () => {
+    if (!globalStore.isCompact) {
+      globalStore.droverOpened = false;
+    }
+  }
+);
 </script>
 <style scoped lang="scss">
 .market-header {
@@ -41,6 +89,11 @@ const { opened: catalogOpened } = storeToRefs(useCatalogStore());
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  &__burger {
+    margin-top: $padding-v;
+    margin-bottom: $padding-v;
   }
 }
 </style>
